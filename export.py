c2bc91b471a40bccb2cb4b91855d68ccf51eab8a
karpov-sv <karpov.sv@gmail.com>
2023-05-25 10:35:01 +0200

    GRANDMA file exporter, to produce calibrated images with proper names and headers, from FRAM archive. Does not change FITS keywords yet, to be changed later

---
+++ export.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os, sys, glob
+
+from astropy.wcs import WCS
+from astropy.io import fits
+from astropy.time import Time
+
+import numpy as np
+
+from fram.fram import Fram
+from fram import calibrate
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    basedir = '~/fram/' # os.path.dirname(__file__)
+
+    parser = OptionParser(usage="usage: %prog [options] from to")
+    parser.add_option('-d', '--db', help='Database name', action='store', dest='db', type='str', default='fram')
+    parser.add_option('-H', '--host', help='Database host', action='store', dest='dbhost', type='str', default=None)
+    parser.add_option('-o', '--out', help='Output directory', action='store', dest='outdir', type='str', default='output')
+    parser.add_option('-n', '--obs-name', help='Observer name', action='store', dest='obsname', type='str', default='Karpov')
+    parser.add_option('-v', '--verbose', help='Verbose', action='store_true', dest='verbose', default=False)
+
+    (options,files) = parser.parse_args()
+
+    fram = None
+
+    for filename in files:
+        image = fits.getdata(filename)
+        header = fits.getheader(filename)
+
+        name = header.get('OBJECT')
+
+        if name.startswith('GRANDMA'):
+            s = name.split('_')
+            event_name = s[1]
+            tile_id = int(s[2])
+        else:
+            continue
+
+        fname = header.get('FILTER', 'Unknown')
+        exposure = header.get('EXPOSURE')
+        nstacked = 1
+
+        ra,dec = header.get('TELRA'), header.get('TELDEC')
+
+        if 'BART' in header.get('INSTRUME'):
+            telname = 'FRAM-CTA-N'
+        else:
+            telname = 'FRAM-Auger'
+
+        utctime = Time(header['DATE-OBS']).strftime('%Y-%m-%dT%H-%M-%S')
+
+        outname = '_'.join([event_name,
+                            options.obsname,
+                            telname,
+                            utctime,
+                            fname,
+                            'STACK' if nstacked>1 else 'UNSTACK',
+                            str(nstacked)+'x'+str(int(exposure))+'s',
+                            ('%03.6f' % ra).replace('.', '-'),
+                            ('%03.6f' % dec).replace('.', '-')
+                            ]) + '.fits'
+
+        outname = os.path.join(options.outdir, outname)
+
+        print(filename, '->', outname)
+
+        try:
+            os.makedirs(options.outdir)
+        except:
+            pass
+
+        if fram is None:
+            fram = Fram(dbname=options.db, dbhost=options.dbhost)
+
+        #### Basic calibration
+        darkname = fram.find_image('masterdark', header=header, debug=False)
+        flatname = fram.find_image('masterflat', header=header, debug=False)
+
+        if options.verbose:
+            print('Dark:', darkname)
+            print('Flat:', flatname)
+
+        if darkname:
+            dark = fits.getdata(basedir + '/' + darkname)
+        else:
+            dcname = fram.find_image('dcurrent', header=header, debug=False)
+            biasname = fram.find_image('bias', header=header, debug=False)
+            if dcname and biasname:
+                bias = fits.getdata(basedir + '/' + biasname)
+                dc = fits.getdata(basedir + '/' + dcname)
+
+                dark = bias + header['EXPOSURE']*dc
+            else:
+                dark = None
+
+        image,header = calibrate.calibrate(image, header, dark=dark)
+
+        if flatname:
+            flat = fits.getdata(basedir + '/' + flatname)
+
+            image *= np.nanmedian(flat)/flat
+
+        fits.writeto(outname, image, header, overwrite=True)
